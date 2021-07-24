9f014e046ef6e658fc2ce653bc245bbd67a1f3fb
ramliracika <ramliracika@gmail.com>
2021-07-24 23:25:46 +0700

    aplikasi pertama say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ramliracika/go-sayHello
+
+go 1.13
+
+require github.com/ramliracika/go-sayHello/v2 v2.1.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/ramliracika/go-sayHello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello(" Raka"))
+}
